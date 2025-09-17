53605f5d4c99849c4851b7e43dc96935b92c5a78
Vasco Ferreira <vasco_mmf@hotmail.com>
2025-09-17 18:59:21 +0100

    feat: create base spring boot project

---
+++ src/main/java/com/example/tinyledger/TinyledgerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinyledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyledgerApplication.class, args);
+	}
+
+}
